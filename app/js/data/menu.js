2a69374d21a31627a668d71e4390d262cb065036
jecode93 <jeanemmanuelcadet@gmail.com>
2023-12-12 21:11:25 -0500

    Add dynamic menu and social icon.

---
+++ app/js/data/menu.js
@@ -0,0 +1,35 @@
+import { BsFacebook, BsLinkedin, BsGithub } from "react-icons/bs";
+
+export const menu = [
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Portfolio",
+    path: "/portfolio",
+  },
+  {
+    name: "Services",
+    path: "/services",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+export const socials = [
+  {
+    link: "#",
+    icon: <BsFacebook />,
+  },
+  {
+    link: "#",
+    icon: <BsLinkedin />,
+  },
+  {
+    link: "#",
+    icon: <BsGithub />,
+  },
+];
