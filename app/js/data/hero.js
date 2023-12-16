00d656a6ea18d0326b7d385955cf90c39d7abd27
jecode93 <jeanemmanuelcadet@gmail.com>
2023-12-16 15:47:52 -0500

    Add hero text for hero section.

---
+++ app/js/data/hero.js
@@ -0,0 +1,10 @@
+export const hero = [
+  {
+    subTitle: "Welcome to my digital space where innovation meets expertise",
+    title: "Experienced Full-Stack Software Engineer",
+    name: "Jean Emmanuel Cadet",
+    desc1:
+      "From frontend to backend mastery, I thrive in turning complex ideas into elegant, user-centric applications.",
+    desc2: 'Let\'s embark on a journey to explore the endless possibilities of code...'
+  },
+];
