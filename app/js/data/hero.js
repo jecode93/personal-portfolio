6bd67d0e3347ce887378880778d9ab18f7327131
jecode93 <jeanemmanuelcadet@gmail.com>
2023-12-17 18:06:08 -0500

    Complete hero section for homepage.

--- app/js/data/hero.js
+++ app/js/data/hero.js
@@ -1,11 +1,12 @@
 export const hero = [
   {
-    subTitle: "Welcome to my digital space where innovation meets expertise",
-    title: "Experienced Full-Stack Software Engineer",
-    name: "Jean Emmanuel Cadet",
+    subTitle: "Welcome to my digital space where innovation meets expertise.",
+    title: "Experienced Full-Stack",
+    title2: "Software Engineer",
     desc1:
       "From frontend to backend mastery, I thrive in turning complex ideas into elegant, user-centric applications.",
-    desc2: 'Let\'s embark on a journey to explore the endless possibilities of code...'
+    desc2:
+      "Let's embark on a journey to explore the endless possibilities of code...",
   },
 ];
 
@@ -20,6 +21,6 @@ export const infos = [
   },
   {
     text: "Work location",
-    content: "Remote",
+    content: "Remotely",
   },
 ];
