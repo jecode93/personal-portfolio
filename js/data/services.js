e2d83a2bea3069ed6a747eb980de2b49518f8b4a
jecode93 <jeanemmanuelcadet@gmail.com>
2023-12-29 17:46:54 -0500

    Change data type for prices.

--- js/data/services.js
+++ js/data/services.js
@@ -4,20 +4,20 @@ export const services = [
     title: "Web development",
     description:
       "Design and development of custom websites, web applications, and single-page applications (SPAs).",
-    price: "40",
+    price: 40,
   },
   {
     icon: "/icons/api.svg",
     title: "API development",
     description:
       "Creating and integrating APIs for seamless data exchange between applications.",
-    price: "50",
+    price: 50,
   },
   {
     icon: "/icons/database.svg",
     title: "Database design and management",
     description:
       "Setting up and managing databases for optimal performance and data security.",
-    price: "45",
+    price: 45,
   },
 ];
