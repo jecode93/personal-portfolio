8e727e79b94ec9fd7e00cef4b02805c060cd5150
jecode93 <jeanemmanuelcadet@gmail.com>
2023-12-31 15:15:43 -0500

    Add footer component to layout, and update description text.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,13 @@
 import './globals.css';
 import { nunito } from "@/js/fonts";
 import Navbar from "@/components/Navbar";
+import Footer from '@/components/Footer';
 
 export const metadata = {
-  title: 'Full-Stack Software Engineer | Jean Emmanuel Cadet',
-  description: 'I am a Full-Stack Software Engineer with over 5 years of experience in web development. I am proficient in all aspects of the development process, from front-end to back-end. If you are looking for a talented and experienced Full-Stack Software Engineer, please contact me today. I would be happy to discuss your project and how I can help you achieve your goals.',
-}
+  title: "Full-Stack Software Engineer | Jean Emmanuel Cadet",
+  description:
+    "I am a Full-Stack Software Engineer with more than 3 years of experience in web development. I am proficient in all aspects of the development process, from Front-end to Back-end mastery, I thrive in turning complex ideas into elegant, user-centric applications.",
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -13,6 +15,7 @@ export default function RootLayout({ children }) {
       <body className={`${nunito.className} bg-blue text-white`}>
         <Navbar />
         <div>{children}</div>
+        <Footer />
       </body>
     </html>
   );
