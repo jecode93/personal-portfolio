813febe5e131475fecc64a8a8d65aa4de4649e1c
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-11 19:38:50 -0500

    Add new home folder to structure js files.

---
+++ js/data/home/portfolio.js
@@ -0,0 +1,91 @@
+export const portfolio = [
+  {
+    title: "Car booking system",
+    image: "",
+    link: "https://final-capstone-frontend-3l0g.onrender.com/",
+    description:
+      "Car Book is a website where users can book a Car. The user can also see the details of a Car, add a car, reserve a car, see reservations, and delete a car created by the user.",
+  },
+  {
+    title: "Country Weather app",
+    image: "",
+    link: "https://react-capstone-project-git-dev-jecode93.vercel.app/",
+    description: `It is a web app for everyone interested in weather and would like to check all the metrics including temperature, wind speed, humidity, etc. It is a Single Page App (SPA) that allows users to:
+
+• Choose a country to dive deeper into its weather metrics.
+• When clicked on a country it displays all the metrics of that country's capital regarding weather.`,
+  },
+  {
+    title: "Leaderboard",
+    image: "",
+    link: "https://leaderboard-jecode93.netlify.app/",
+    description: "",
+  },
+  {
+    title: "Math-magician",
+    image: "",
+    link: "https://math-magicians-dk01.onrender.com/calculator",
+    description: "",
+  },
+  {
+    title: "Space Travelers",
+    image: "",
+    link: "https://space-travelers-hub-uro6.onrender.com/",
+    description:
+      "Space Travelers' Hub is a single-page application (SPA), where users can book rockets and join selected space missions of SpaceX. STH uses real live data from the SpaceX API.",
+  },
+  {
+    title: "",
+    image: "",
+    link: "https://idasboiserie.com/",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    link: "https://clindoeilinfo.com/",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    link: "https://jeanemmanuelcadet.com/",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    link: "https://new-todolist93.netlify.app/",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    link: "http://cdeph.org/",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    link: "http://cdeph.org/",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    link: "http://cdeph.org/",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    link: "http://cdeph.org/",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    link: "http://cdeph.org/",
+    description: "",
+  },
+];
