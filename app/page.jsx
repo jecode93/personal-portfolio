fb0d70ce1abe6e6178b41d752468e23a4eb1e8ea
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-13 21:14:13 -0500

    Add portfolio section.

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Home/HeroSection";
 import About from "@/components/Home/AboutSection";
 import ServicesSection from "@/components/Home/ServicesSection";
+import PortfolioSection from "@/components/Home/PortfolioSection";
 
 export default function Home() {
   return (
@@ -8,7 +9,8 @@ export default function Home() {
       <main>
         <Hero /> {/* Hero section */}
         <About /> {/* About section */}
-        <ServicesSection /> {/* About section */}
+        <ServicesSection /> {/* Services section */}
+        <PortfolioSection /> {/* Portfolio section */}
       </main>
     </>
   );
