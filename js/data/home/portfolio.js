f5d7328b0d345e1ab48657eb38b32410d9ee349c
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-13 23:24:08 -0500

    Update portfolio data informations.

--- js/data/home/portfolio.js
+++ js/data/home/portfolio.js
@@ -1,91 +1,83 @@
 export const portfolio = [
   {
-    title: "Car booking system",
-    image: "",
+    id: 1,
+    title: "Car Booking System",
+    image: "/portfolio/bookcar.jpeg",
     link: "https://final-capstone-frontend-3l0g.onrender.com/",
     description:
       "Car Book is a website where users can book a Car. The user can also see the details of a Car, add a car, reserve a car, see reservations, and delete a car created by the user.",
+    stack:
+      "JavaScript • React • Redux • Ruby • Ruby on Rails • PostgreSQL • Tailwind CSS",
   },
   {
-    title: "Country Weather app",
-    image: "",
+    id: 2,
+    title: "Space Travelers Hub",
+    image: "/portfolio/falcon.jpeg",
+    link: "https://space-travelers-hub-uro6.onrender.com/",
+    description:
+      "Space Travelers' Hub is a single-page application (SPA), where users can book rockets and join selected space missions of SpaceX. STH uses real live data from the SpaceX API.",
+    stack: "JavaScript • React • Redux • API Integration • CSS",
+  },
+  {
+    id: 3,
+    title: "Country Weather App",
+    image: "/portfolio/weather.jpeg",
     link: "https://react-capstone-project-git-dev-jecode93.vercel.app/",
     description: `It is a web app for everyone interested in weather and would like to check all the metrics including temperature, wind speed, humidity, etc. It is a Single Page App (SPA) that allows users to:
 
 • Choose a country to dive deeper into its weather metrics.
 • When clicked on a country it displays all the metrics of that country's capital regarding weather.`,
+    stack: "JavaScript • React • Redux • Tailwind CSS",
   },
   {
-    title: "Leaderboard",
+    id: 4,
+    title: "InvesTO",
     image: "",
-    link: "https://leaderboard-jecode93.netlify.app/",
-    description: "",
-  },
-  {
-    title: "Math-magician",
-    image: "",
-    link: "https://math-magicians-dk01.onrender.com/calculator",
-    description: "",
-  },
-  {
-    title: "Space Travelers",
-    image: "",
-    link: "https://space-travelers-hub-uro6.onrender.com/",
+    link: "https://github.com/jecode93/budget-app",
     description:
-      "Space Travelers' Hub is a single-page application (SPA), where users can book rockets and join selected space missions of SpaceX. STH uses real live data from the SpaceX API.",
+      "InvesTO is a mobile web application where you can manage your budget: you have a list of transactions associated with a category so that you can see how much money you spend and on what.",
+    stack: "Ruby • Ruby on Rails • PostgreSQL • Tailwind CSS",
   },
   {
-    title: "",
-    image: "",
+    id: 5,
+    title: "Leaderboard",
+    image: "/portfolio/leaderboard.png",
+    link: "https://leaderboard-jecode93.netlify.app/",
+    description:
+      "A JavaScript project  that shows some scores for a specific user. This Leaderboard app is built using webpack and ES6 features. It consume the Leaderboard API using JavaScript async and await.",
+    stack: "HTML • CSS • JavaScript  • Webpack • SCSS",
+  },
+  {
+    id: 6,
+    title: "Math-magician",
+    image: "/portfolio/math-magicians.png",
+    link: "https://math-magicians-dk01.onrender.com/calculator",
+    description:
+      "Math Magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: - Make simple calculations. - Read a random math-related quote.",
+    stack: "JavaScript • React • Redux • PostgreSQL • CSS",
+  },
+  {
+    id: 7,
+    title: "Idas Boiserie",
+    image: "/portfolio/idas.jpeg",
     link: "https://idasboiserie.com/",
-    description: "",
+    description:
+      "This is a website that I created for a client to showcase his works and sell products.",
   },
   {
-    title: "",
-    image: "",
+    id: 8,
+    title: "Clin D'oeil Info",
+    image: "/portfolio/cdi.jpeg",
     link: "https://clindoeilinfo.com/",
-    description: "",
+    description:
+      "This is a blog I created for a client 3 years ago, to provide national and international news, it has the functionality to create new posts, delete, edit and add other users to help manage the blog.",
   },
   {
-    title: "",
-    image: "",
-    link: "https://jeanemmanuelcadet.com/",
-    description: "",
-  },
-  {
-    title: "",
-    image: "",
-    link: "https://new-todolist93.netlify.app/",
-    description: "",
-  },
-  {
-    title: "",
-    image: "",
-    link: "http://cdeph.org/",
-    description: "",
-  },
-  {
-    title: "",
-    image: "",
-    link: "http://cdeph.org/",
-    description: "",
-  },
-  {
-    title: "",
-    image: "",
-    link: "http://cdeph.org/",
-    description: "",
-  },
-  {
-    title: "",
-    image: "",
-    link: "http://cdeph.org/",
-    description: "",
-  },
-  {
-    title: "",
-    image: "",
-    link: "http://cdeph.org/",
-    description: "",
-  },
+    id: 9,
+    title: "Personal Portfolio",
+    image: "/portfolio/personal.jpeg",
+    link: "https://jeanemmanuelcadet.vercel.com/",
+    description:
+      "How can I list the projects I have already done without putting my project in it, it was a challenge for me to create this website from scratch, which gives me more opportunities to prove my abilities to take a project from scratch to completion.",
+  }
 ];
