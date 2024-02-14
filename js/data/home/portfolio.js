84daf61d9e389c0fdd9bd43fcfdee5458c8b18d4
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-14 00:53:01 -0500

    Change images extension.

--- js/data/home/portfolio.js
+++ js/data/home/portfolio.js
@@ -2,7 +2,7 @@ export const portfolios = [
   {
     id: 1,
     title: "Car Booking System",
-    image: "/portfolio/bookcar.jpeg",
+    image: "/portfolio/bookcar.jpg",
     link: "https://final-capstone-frontend-3l0g.onrender.com/",
     description:
       "Car Book is a website where users can book a Car. The user can also see the details of a Car, add a car, reserve a car, see reservations, and delete a car created by the user.",
@@ -12,7 +12,7 @@ export const portfolios = [
   {
     id: 2,
     title: "Space Travelers Hub",
-    image: "/portfolio/falcon.jpeg",
+    image: "/portfolio/falcon.jpg",
     link: "https://space-travelers-hub-uro6.onrender.com/",
     description:
       "Space Travelers' Hub is a single-page application (SPA), where users can book rockets and join selected space missions of SpaceX. STH uses real live data from the SpaceX API.",
@@ -21,7 +21,7 @@ export const portfolios = [
   {
     id: 3,
     title: "Country Weather App",
-    image: "/portfolio/weather.jpeg",
+    image: "/portfolio/weather.jpg",
     link: "https://react-capstone-project-git-dev-jecode93.vercel.app/",
     description: `It is a web app for everyone interested in weather and would like to check all the metrics including temperature, wind speed, humidity, etc. It is a Single Page App (SPA) that allows users to:
 
@@ -32,7 +32,7 @@ export const portfolios = [
   {
     id: 4,
     title: "InvesTO",
-    image: "/portfolio/bookcar.jpeg",
+    image: "/portfolio/bookcar.jpg",
     link: "https://github.com/jecode93/budget-app",
     description:
       "InvesTO is a mobile web application where you can manage your budget: you have a list of transactions associated with a category so that you can see how much money you spend and on what.",
@@ -41,7 +41,7 @@ export const portfolios = [
   {
     id: 5,
     title: "Leaderboard",
-    image: "/portfolio/leaderboard.jpeg",
+    image: "/portfolio/leaderboard.jpg",
     link: "https://leaderboard-jecode93.netlify.app/",
     description:
       "A JavaScript project  that shows some scores for a specific user. This Leaderboard app is built using webpack and ES6 features. It consume the Leaderboard API using JavaScript async and await.",
@@ -50,7 +50,7 @@ export const portfolios = [
   {
     id: 6,
     title: "Math-magician",
-    image: "/portfolio/math-magicians.jpeg",
+    image: "/portfolio/math-magicians.jpg",
     link: "https://math-magicians-dk01.onrender.com/calculator",
     description:
       "Math Magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: - Make simple calculations. - Read a random math-related quote.",
@@ -59,7 +59,7 @@ export const portfolios = [
   {
     id: 7,
     title: "Idas Boiserie",
-    image: "/portfolio/idas.jpeg",
+    image: "/portfolio/idas.jpg",
     link: "https://idasboiserie.com/",
     description:
       "This is a website that I created for a client to showcase his works and sell products.",
@@ -68,7 +68,7 @@ export const portfolios = [
   {
     id: 8,
     title: "Clin D'oeil Info",
-    image: "/portfolio/cdi.jpeg",
+    image: "/portfolio/cdi.jpg",
     link: "https://clindoeilinfo.com/",
     description:
       "This is a blog I created for a client 3 years ago, to provide national and international news, it has the functionality to create new posts, delete, edit and add other users to help manage the blog.",
@@ -77,7 +77,7 @@ export const portfolios = [
   {
     id: 9,
     title: "Personal Portfolio",
-    image: "/portfolio/personal.jpeg",
+    image: "/portfolio/personal.jpg",
     link: "https://jeanemmanuelcadet.vercel.com/",
     description:
       "How can I list the projects I have already done without putting my project in it, it was a challenge for me to create this website from scratch, which gives me more opportunities to prove my abilities to take a project from scratch to completion.",
