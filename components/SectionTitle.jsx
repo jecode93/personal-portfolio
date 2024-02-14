e6cb3414b63ff75d9e65959ef35490105d3107a0
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-13 20:53:32 -0500

    Create Section Title component.

---
+++ components/SectionTitle.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SectionTitle = ({ title, text }) => {
+  return (
+    <>
+      <h2 className="uppercase tracking-widest md:text-xl text-orange-500 mb-10">
+        {title}
+      </h2>
+      <div className="mb-20">
+        <h1 className="font-bold text-3xl md:text-5xl mb-5">{text}</h1>
+        <div className="w-20 md:w-40 border" />
+      </div>
+    </>
+  );
+}
+
+export default SectionTitle
