2fb19818ff7f0b797cd93b95c5889165e2dd9fab
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-13 21:02:11 -0500

    Add Section Title component and update bullet image path.

--- components/Home/ServicesSection.jsx
+++ components/Home/ServicesSection.jsx
@@ -2,34 +2,45 @@ import Image from 'next/image';
 import React from 'react';
 import { services } from "@/js/data/home/services";
 import Link from 'next/link';
+import SectionTitle from '../SectionTitle';
+import bullet from "@/public/images/bullet.png";
 
 const ServicesSection = () => {
   return (
     <section id="services" className="px-5 md:px-10 lg:px-32 py-20 relative">
-      <div className='z-0'>
+      <div className="z-0">
         <Image
-          src="/images/bullet.png"
+          src={bullet}
           alt="Bullet"
           width={100}
           height={100}
-          className="absolute right-0 -top-20"
+          className="absolute right-0 -top-20 w-[100]"
         />
       </div>
-      <h2 className="uppercase tracking-widest md:text-xl text-orange-500 mb-10">
-        Services
-      </h2>
-      <div className="mb-20">
-        <h1 className="font-bold text-3xl md:text-5xl mb-5">What I Do</h1>
-        <div className="w-20 md:w-40 border" />
-      </div>
+      <SectionTitle title="Services" text="What I Do" />
       <div className="grid md:grid-cols-1 lg:grid-cols-3 place-content-center gap-5 z-40">
         {services.map((service, index) => (
-          <div key={index} className="bg-darkblue p-5 md:p-10 flex flex-col justify-between rounded">
+          <div
+            key={index}
+            className="bg-darkblue p-5 md:p-10 flex flex-col justify-between rounded"
+          >
             <Link href="/services">
-              <Image src={service.icon} width={70} height={70} alt={service.title} className='mb-10 w-12 md:w-16'/>
-              <h2 className="font-bold text-xl md:text-2xl mb-5">{service.title}</h2>
-              <p className='opacity-60 mb-10 md:text-xl'>{service.description}</p>
-              <p className='text-xl font-bold text-orange-500'>${service.price}/h</p>
+              <Image
+                src={service.icon}
+                width={70}
+                height={70}
+                alt={service.title}
+                className="mb-10 w-12 md:w-16"
+              />
+              <h2 className="font-bold text-xl md:text-2xl mb-5">
+                {service.title}
+              </h2>
+              <p className="opacity-60 mb-10 md:text-xl">
+                {service.description}
+              </p>
+              <p className="text-xl font-bold text-orange-500">
+                ${service.price}/h
+              </p>
             </Link>
           </div>
         ))}
