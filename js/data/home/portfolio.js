d57953fa028f58d28eb742fd068312cd8d6dd85f
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-14 00:32:31 -0500

    Update images path and change data name.

--- js/data/home/portfolio.js
+++ js/data/home/portfolio.js
@@ -1,4 +1,4 @@
-export const portfolio = [
+export const portfolios = [
   {
     id: 1,
     title: "Car Booking System",
@@ -32,7 +32,7 @@ export const portfolio = [
   {
     id: 4,
     title: "InvesTO",
-    image: "",
+    image: "/portfolio/bookcar.jpeg",
     link: "https://github.com/jecode93/budget-app",
     description:
       "InvesTO is a mobile web application where you can manage your budget: you have a list of transactions associated with a category so that you can see how much money you spend and on what.",
@@ -41,7 +41,7 @@ export const portfolio = [
   {
     id: 5,
     title: "Leaderboard",
-    image: "/portfolio/leaderboard.png",
+    image: "/portfolio/leaderboard.jpeg",
     link: "https://leaderboard-jecode93.netlify.app/",
     description:
       "A JavaScript project  that shows some scores for a specific user. This Leaderboard app is built using webpack and ES6 features. It consume the Leaderboard API using JavaScript async and await.",
@@ -50,7 +50,7 @@ export const portfolio = [
   {
     id: 6,
     title: "Math-magician",
-    image: "/portfolio/math-magicians.png",
+    image: "/portfolio/math-magicians.jpeg",
     link: "https://math-magicians-dk01.onrender.com/calculator",
     description:
       "Math Magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: - Make simple calculations. - Read a random math-related quote.",
@@ -63,6 +63,7 @@ export const portfolio = [
     link: "https://idasboiserie.com/",
     description:
       "This is a website that I created for a client to showcase his works and sell products.",
+    stack: "WordPress • Elementor Page Builder",
   },
   {
     id: 8,
@@ -71,6 +72,7 @@ export const portfolio = [
     link: "https://clindoeilinfo.com/",
     description:
       "This is a blog I created for a client 3 years ago, to provide national and international news, it has the functionality to create new posts, delete, edit and add other users to help manage the blog.",
+    stack: "WordPress • Elementor Page Builder",
   },
   {
     id: 9,
@@ -79,5 +81,6 @@ export const portfolio = [
     link: "https://jeanemmanuelcadet.vercel.com/",
     description:
       "How can I list the projects I have already done without putting my project in it, it was a challenge for me to create this website from scratch, which gives me more opportunities to prove my abilities to take a project from scratch to completion.",
-  }
+    stack: "JavaScript • React • Next.js • Talwind CSS",
+  },
 ];
