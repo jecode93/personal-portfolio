94bea6dd44e8724a8b39fdb8fbe2b923f75b210a
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-18 17:00:33 -0500

    Update each portfolio description.

--- js/data/home/portfolio.js
+++ js/data/home/portfolio.js
@@ -4,30 +4,69 @@ export const portfolios = [
     title: "Car Booking System",
     image: "/portfolio/bookcar.jpg",
     link: "https://final-capstone-frontend-3l0g.onrender.com/",
-    description:
-      "Car Book is a website where users can book a Car. The user can also see the details of a Car, add a car, reserve a car, see reservations, and delete a car created by the user.",
-    stack:
-      "JavaScript • React • Redux • Ruby • Ruby on Rails • PostgreSQL • Tailwind CSS",
+    description: `
+Car Book revolutionizes car rentals, bridging the gap between renters and owners with a user-centric platform.
+
+Streamlined Rentals for Renters:
+
+• Effortless Browsing: Explore a diverse selection of cars based on your specific needs, filtering by type, size, and features.
+
+• Informed Decisions: Make confident choices with detailed car information, high-quality images, and transparent pricing.
+
+• Seamless Booking: Secure your ideal car with a user-friendly reservation system, ensuring a smooth rental experience.
+
+Empowering Car Owners:
+
+• Centralized Management: Manage listings, set availability, and track reservations – all from a convenient dashboard.
+
+• Effortless Listing: Add your car with ease, providing detailed descriptions and captivating photos.
+
+• Building Trust: Foster transparency with features like car addition/removal and user reviews.
+
+Beyond Convenience:
+
+Car Book transcends a simple booking platform, fostering a community built on trust and transparency. It empowers both renters and owners, saving time, simplifying the process, and offering a seamless experience for all.`,
+    stack: [
+      "JavaScript",
+      "React",
+      "Redux",
+      "Ruby",
+      "Ruby on Rails",
+      "PostgreSQL",
+      "Tailwind CSS",
+    ],
   },
   {
     id: 2,
     title: "Space Travelers Hub",
     image: "/portfolio/falcon.jpg",
     link: "https://space-travelers-hub-uro6.onrender.com/",
-    description:
-      "Space Travelers' Hub is a single-page application (SPA), where users can book rockets and join selected space missions of SpaceX. STH uses real live data from the SpaceX API.",
-    stack: "JavaScript • React • Redux • API Integration • CSS",
+    description: `Space Travelers' Hub (STH) is a dynamic single-page application (SPA) that ignites your dreams of space exploration. Powered by real-time data from the SpaceX API, STH empowers you to:
+
+• Explore and book rockets: Browse through available SpaceX rockets, delve into their specifications, and seamlessly secure your reservation for an unforgettable celestial journey.
+
+• Join captivating missions: Discover a curated selection of thrilling space missions and become an active participant in pushing the boundaries of human exploration.
+
+STH transcends a simple booking platform, fostering a vibrant community of space enthusiasts united by a shared passion for discovery.`,
+    stack: ["JavaScript", "React", "Redux", "API Integration", "CSS"],
   },
   {
     id: 3,
     title: "Country Weather App",
     image: "/portfolio/weather.jpg",
     link: "https://react-capstone-project-git-dev-jecode93.vercel.app/",
-    description: `It is a web app for everyone interested in weather and would like to check all the metrics including temperature, wind speed, humidity, etc. It is a Single Page App (SPA) that allows users to:
+    description: `This captivating single-page application (SPA) caters to every weather enthusiast, providing a comprehensive and convenient platform to explore a world of meteorological data.
 
-• Choose a country to dive deeper into its weather metrics.
-• When clicked on a country it displays all the metrics of that country's capital regarding weather.`,
-    stack: "JavaScript • React • Redux • Tailwind CSS",
+Key Features:
+
+• Global Reach: Delve into weather conditions across the globe, choosing any country to unlock a treasure trove of insights.
+
+• Granular Details: Dive deeper into each chosen country, accessing real-time weather metrics for its capital city, including temperature, wind speed, humidity, and more.
+
+Beyond Information:
+
+This user-friendly app empowers you to stay informed and make informed decisions, whether planning a trip, preparing for outdoor activities, or simply staying curious about the ever-changing world around you.`,
+    stack: ["JavaScript", "React", "Redux", "Tailwind CSS"],
   },
   // {
   //   id: 4,
@@ -36,43 +75,77 @@ export const portfolios = [
   //   link: "https://github.com/jecode93/budget-app",
   //   description:
   //     "InvesTO is a mobile web application where you can manage your budget: you have a list of transactions associated with a category so that you can see how much money you spend and on what.",
-  //   stack: "Ruby • Ruby on Rails • PostgreSQL • Tailwind CSS",
+  //   stack: ["Ruby", "Ruby on Rails", "PostgreSQL", "Tailwind CSS"],
   // },
   {
     id: 5,
     title: "Leaderboard",
     image: "/portfolio/leaderboard.jpg",
     link: "https://leaderboard-jecode93.netlify.app/",
-    description:
-      "A JavaScript project  that shows some scores for a specific user. This Leaderboard app is built using webpack and ES6 features. It consume the Leaderboard API using JavaScript async and await.",
-    stack: "HTML • CSS • JavaScript  • Webpack • SCSS",
+    description: `This compelling JavaScript project, built with cutting-edge Webpack and ES6 features, unveils a dynamic leaderboard experience. It seamlessly retrieves data from the Leaderboard API using the power of asynchronous operations (async/await), ensuring a smooth and efficient user experience.
+
+Key Features:
+
+• Live Score Display: Witness your scores prominently displayed, keeping you motivated and informed about your progress.
+
+• Modern Tech Stack: Experience the benefits of Webpack and ES6, ensuring a robust and future-proof development foundation.
+
+• Asynchronous Data Fetching: Enjoy seamless data retrieval from the Leaderboard API, powered by efficient async/await functionality.
+
+Beyond Scores:
+
+This project transcends a simple score display, fostering a sense of competition and community among users striving to reach the top.`,
+    stack: ["HTML", "CSS", "JavaScript", "Webpack", "SCSS"],
   },
   {
     id: 6,
     title: "Math-magician",
     image: "/portfolio/math-magicians.jpg",
     link: "https://math-magicians-dk01.onrender.com/calculator",
-    description:
-      "Math Magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: - Make simple calculations. - Read a random math-related quote.",
-    stack: "JavaScript • React • Redux • PostgreSQL • CSS",
+    description: `Math Magicians is a captivating single-page application (SPA) designed to ignite your passion for mathematics. This interactive platform caters to learners of all levels, offering two key features:
+
+• Effortless Calculations: Perform basic mathematical operations with ease, making it a handy tool for quick calculations or practicing your numerical skills.
+
+• Daily Dose of Inspiration: Discover a new thought-provoking math quote each day, sparking your curiosity and igniting your fascination with the beauty and power of numbers.
+
+Math Magicians goes beyond a simple calculator, fostering a love for mathematics and encouraging exploration in a fun and engaging way.`,
+    stack: ["JavaScript", "React", "Redux", "PostgreSQL", "CSS"],
   },
   {
     id: 7,
     title: "Idas Boiserie",
     image: "/portfolio/idas.jpg",
     link: "https://idasboiserie.com/",
-    description:
-      "This is a website that I created for a client to showcase his works and sell products.",
-    stack: "WordPress • Elementor Page Builder",
+    description: `This website serves as a captivating platform, meticulously crafted to showcase your client's exceptional work and seamlessly facilitate product sales. It caters to both potential clients and loyal customers, offering a comprehensive and engaging experience.
+
+Key Features:
+
+• Dazzling Portfolio: Immerse visitors in a stunning display of your client's work, highlighting their expertise and creative vision.
+
+• Seamless Shopping Experience: Navigate a user-friendly interface designed to streamline the product purchasing process, ensuring customer satisfaction.
+
+Beyond Functionality:
+
+This website transcends a simple online store, fostering a lasting connection between your client and their audience. It serves as a powerful tool to build brand recognition, cultivate customer loyalty, and drive successful sales.`,
+    stack: ["WordPress", "Elementor Page Builder"],
   },
   {
     id: 8,
     title: "Clin D'oeil Info",
     image: "/portfolio/cdi.jpg",
     link: "https://clindoeilinfo.com/",
-    description:
-      "This is a blog I created for a client 3 years ago, to provide national and international news, it has the functionality to create new posts, delete, edit and add other users to help manage the blog.",
-    stack: "WordPress • Elementor Page Builder",
+    description: `Three years ago, I created a dynamic platform to empower informed citizens, and this client's news blog stands as a testament to that vision. This comprehensive platform transcends a simple news feed, offering a robust suite of features:
+
+• National and International News: Stay abreast of current events, both locally and globally, with access to a diverse range of news articles.
+
+• Content Management Powerhouse: Enjoy the flexibility to create, edit, and delete posts, ensuring the blog remains current and relevant.
+
+• Collaborative Spirit: Foster a collaborative environment by adding other users to the management team, allowing for diverse perspectives and efficient content creation.
+
+Beyond the Headlines:
+
+This blog serves not only as a source of information but also as a platform for open dialogue and critical thinking. It empowers readers to engage with the news, form informed opinions, and participate in meaningful discussions.`,
+    stack: ["WordPress", "Elementor Page Builder"],
   },
   {
     id: 9,
@@ -81,6 +154,6 @@ export const portfolios = [
     link: "https://jeanemmanuelcadet.vercel.com/",
     description:
       "How can I list the projects I have already done without putting my project in it, it was a challenge for me to create this website from scratch, which gives me more opportunities to prove my abilities to take a project from scratch to completion.",
-    stack: "JavaScript • React • Next.js • Talwind CSS",
+    stack: ["JavaScript", "React", "Next.js", "Talwind CSS"],
   },
 ];
