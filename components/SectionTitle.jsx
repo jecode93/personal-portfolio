097ca7e5f70c6f34cf45cf157d3a03245df50b31
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-18 23:06:09 -0500

    Remove border opacity, change border color.

--- components/SectionTitle.jsx
+++ components/SectionTitle.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const SectionTitle = ({ title, text }) => {
   return (
     <>
-      <h2 className="uppercase text-[50px] mb-5 font-black tracking-widest md:text-[100px] lg:text-[150px] opacity-20">
+      <h2 className="uppercase text-[50px] md:-mb-6 font-black tracking-widest md:text-[100px] lg:text-[150px] opacity-20">
         {title}
       </h2>
       <div className="mb-20">
-        <h1 className="font-bold text-3xl md:text-5xl mb-5">{text}</h1>
-        <div className="w-20 md:w-40 border opacity-40" />
+        <h1 className="font-bold text-3xl md:text-5xl mb-7">{text}</h1>
+        <div className="w-20 md:w-20 border border-orange-500" />
       </div>
     </>
   );
