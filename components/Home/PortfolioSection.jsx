31fcb113b0b8ea9fe34a72665adb214261ceb2fd
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-18 23:07:03 -0500

    Add swiper for each portfolio.

--- components/Home/PortfolioSection.jsx
+++ components/Home/PortfolioSection.jsx
@@ -4,33 +4,79 @@ import SectionTitle from '../SectionTitle';
 import SectionsComponent from '../SectionsComponent';
 import { portfolios } from '@/js/data/home/portfolio';
 import Image from 'next/image';
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { Autoplay, FreeMode, Pagination } from 'swiper/modules'
+import PopupComponent from './PopupComponent';
 
 const PortfolioSection = () => {
   const [open, setOpen] = useState(false);
-  
-  const popup = (data) => {
-    
-  }
+  const [currentPopup, setCurrentPopup] = useState(null);
+
+  const showModal = (popupId) => {
+    setOpen(true);
+    const popupData = portfolios.find((item) => item.id === popupId);
+    if (popupData) {
+      setCurrentPopup(popupData);
+    }
+  };
   
   return (
     <SectionsComponent id="portfolio" otherClasses="">
       <SectionTitle title="Portfolio" text="Featured Projects" />
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10">
+      <Swiper
+        breakpoints={{
+          390: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+          },
+        }}
+        freeMode={true}
+        pagination={{ clickable: true }}
+        modules={[Autoplay, FreeMode, Pagination]}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        autoHeight={true}
+        speed={600}
+      >
         {portfolios.map((portfolio) => (
-          <div key={portfolio.id} className="bg-darkblue rounded">
-            <Image
-              src={portfolio.image}
-              alt={portfolio.title}
-              width={500}
-              height={500}
-              className="mb-3 rounded-t w-full"
-            />
-            <div className="p-3">
-              <h1 className="text-2xl">{portfolio.title}</h1>
+          <SwiperSlide key={portfolio.id} className="duration-500">
+            <div
+              className="bg-darkblue rounded hover:cursor-pointer duration-200 max-w-sm"
+              onClick={() => showModal(portfolio.id)}
+            >
+              <Image
+                src={portfolio.image}
+                alt={portfolio.title}
+                width={500}
+                height={500}
+                className="mb-3 rounded-t w-full"
+              />
+              <div className="p-3">
+                <h1 className="text-2xl">{portfolio.title}</h1>
+              </div>
             </div>
-          </div>
+          </SwiperSlide>
         ))}
-      </div>
+        {currentPopup && (
+          <PopupComponent
+            isVisible={open}
+            onClose={() => setOpen(false)}
+            content={currentPopup}
+          />
+        )}
+      </Swiper>
     </SectionsComponent>
   );
 }
