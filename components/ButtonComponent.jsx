4887452cd32a65b9edaa1645ba81c0adacc6720f
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-21 12:52:50 -0500

    Change button component style.

--- components/ButtonComponent.jsx
+++ components/ButtonComponent.jsx
@@ -5,7 +5,7 @@ const ButtonComponent = ({ path, text }) => {
   return (
     <Link
       href={path}
-      className="w-fit text-xs mt-5 bg-orange-500 hover:bg-orange-600 px-5 py-2 duration-300 rounded uppercase"
+      className="w-fit text-sm mt-5 font-bold border border-orange-500 text-orange-500 hover:bg-orange-500 hover:text-white px-3 py-1 duration-300 rounded uppercase"
     >
       {text}
     </Link>
