bc02b898a865940356930b5b464a54d7f428e1a9
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-23 09:52:08 -0500

    Add metadata for better SEO result.

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+export const metadata = {
+  title: "Contact me",
+  description: "Ready to collaborate on your next project? Individuals, startups, and businesses alike are welcome! I'd love to hear about your vision and how my full-stack expertise can bring it to life. Contact me today for a free consultation!"
+};
+
 const Contact = () => {
   return (
     <div>Contact</div>
