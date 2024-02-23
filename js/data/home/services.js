f238cb225a742555bd16ba26b70ca0c4702e8398
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-23 09:46:58 -0500

    Update description text.

--- js/data/home/services.js
+++ js/data/home/services.js
@@ -3,21 +3,21 @@ export const services = [
     icon: "/icons/web.svg",
     title: "Web development",
     description:
-      "Design and development of custom websites, web applications, and single-page applications (SPAs).",
+      "From landing pages to complex applications, I create custom web solutions that engage your audience",
     price: 40,
   },
   {
     icon: "/icons/api.svg",
     title: "API development",
     description:
-      "Creating and integrating APIs for seamless data exchange between applications.",
+      "Connect your systems & unlock data with secure, scalable APIs tailored to your needs.",
     price: 50,
   },
   {
     icon: "/icons/database.svg",
     title: "Database design and management",
     description:
-      "Setting up and managing databases for optimal performance and data security.",
+      "Design & manage robust databases that store & organize your critical data efficiently.",
     price: 45,
   },
 ];
