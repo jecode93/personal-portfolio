bff01fbbc49e34dc1a0bb14847174dee3b55fc02
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-25 03:00:10 -0500

    Fix slide overflow hidden.

--- components/Home/PortfolioSection.jsx
+++ components/Home/PortfolioSection.jsx
@@ -54,7 +54,7 @@ const PortfolioSection = () => {
         {portfolios.map((portfolio) => (
           <SwiperSlide key={portfolio.id} className="duration-500">
             <div
-              className="flex flex-col justify-between bg-darkblue rounded-t-lg hover:cursor-pointer duration-200 max-w-md h-[330px] lg:h-[290px]"
+              className="flex flex-col justify-between bg-darkblue rounded-t-lg hover:cursor-pointer duration-200 max-w-md h-[330px] lg:h-full"
               onClick={() => showModal(portfolio.id)}
             >
               <Image
