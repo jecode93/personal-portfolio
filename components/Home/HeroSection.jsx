0aecb1ec6bacb7faa8cd9ae957a14ab50a101821
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-25 18:33:40 -0500

    Add background image to section instead of a div.

--- components/Home/HeroSection.jsx
+++ components/Home/HeroSection.jsx
@@ -5,11 +5,12 @@ import ButtonComponent from "../ButtonComponent";
 
 const Hero = () => {
   return (
-    <section id="home" className="h-screen">
+    <section
+      id="home"
+      className="h-screen bg-hero w-screen bg-center bg-cover bg-no-repeat bg-fixed"
+    >
       <div className="relative">
-        <div className="bg-hero w-screen bg-center h-screen bg-cover bg-no-repeat bg-fixed absolute " />
         <div className="bg-gradient-to-t from-blue w-screen h-screen absolute z-0" />
-        {/* <div className="bg-gradient-to-r from-blue w-screen h-screen absolute z-0" /> */}
       </div>
       <div className="md:px-5 lg:px-32 h-screen flex flex-col justify-between">
         <div className="flex flex-col px-5 tracking-widest leading-7 justify-center lg:w-[75%] h-full pt-16 z-20">
