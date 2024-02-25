02f29c8499b135787db98f00b798bfb830c132ff
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-25 03:05:38 -0500

    Create cta component file.

---
+++ components/Home/Cta.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SectionsComponent from '../SectionsComponent';
+
+const Cta = () => {
+  return (
+    <SectionsComponent id="Call to action" otherClasses="bg-orange-500">Call to action</SectionsComponent>
+  )
+}
+
+export default Cta;
