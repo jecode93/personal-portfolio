85a7e27776f416aa3f5b0280041d5421f8df6183
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-27 20:44:29 -0500

    Update footer font size.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <>
       <footer>
         <div className="md:flex justify-between items-center text-center py-7 px-5 md:px-10 lg:px-32">
-          <p className="mb-5 md:mb-0 text-xs md:text-sm text-degrade tracking-widest">
+          <p className="mb-5 md:mb-0 text-sm md:text-md text-degrade tracking-widest">
             Copyright © 2023 - {currentYear}. All right reserved
           </p>
           <SocialComponent />
