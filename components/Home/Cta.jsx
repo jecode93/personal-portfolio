eb430f41f440fa1270ea4f710c8a5fdd39509f44
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-27 20:16:10 -0500

    Completed CTA section.

--- components/Home/Cta.jsx
+++ components/Home/Cta.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import SectionsComponent from '../SectionsComponent';
+import Link from "next/link";
 
 const Cta = () => {
   return (
-    <SectionsComponent id="Call to action" otherClasses="bg-orange-500">Call to action</SectionsComponent>
-  )
-}
+    <SectionsComponent
+      id="call_to_action"
+      otherClasses="bg-orange-500 text-center text-blue"
+    >
+      <h2 className="text-3xl md:text-5xl font-extrabold text-blue">
+        Let's Build it. Together.
+      </h2>
+      <p className="my-7 md:mx-5 lg:mx-32 text-xl tracking-wider">
+        As a seasoned full-stack software engineer, I bring innovation and
+        proficiency to every project I undertake. Whether you're a startup in
+        need of a dynamic web application or an established enterprise seeking
+        cutting-edge software solutions, I'm here to turn your vision into
+        reality. Let's collaborate to create tailored solutions that exceed your
+        expectations and propel your business forward.
+      </p>
+      <Link
+        href="contact"
+        className="px-3 py-2 bg-blue text-sm font-bold text-white uppercase hover:bg-bluethin duration-300 rounded"
+      >
+        Start today
+      </Link>
+    </SectionsComponent>
+  );
+};
 
 export default Cta;
