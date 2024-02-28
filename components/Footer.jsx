ea75af42959b38e6ab1a460d5b279fa4923bedba
jecode93 <jeanemmanuelcadet@gmail.com>
2024-02-27 20:10:20 -0500

    Add social media component to the footer.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SocialComponent from "./SocialComponent";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -6,10 +7,11 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <div className="text-center py-5 px-5">
-          <p className="text-xs md:text-sm text-degrade tracking-widest">
+        <div className="md:flex justify-between items-center text-center py-7 px-5 md:px-10 lg:px-32">
+          <p className="mb-5 md:mb-0 text-xs md:text-sm text-degrade tracking-widest">
             Copyright © 2023 - {currentYear}. All right reserved
           </p>
+          <SocialComponent />
         </div>
       </footer>
     </>
