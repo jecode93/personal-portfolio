cebd8e67d892ffe3a8780d4a4c483f5553c67fc2
jecode93 <jeanemmanuelcadet@gmail.com>
2024-03-01 22:34:46 -0500

    Update copyright font-size in footer.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <>
       <footer>
         <div className="flex flex-col-reverse gap-7 md:flex-row justify-between items-center text-center py-7 px-5 md:px-10 lg:px-32">
-          <p className="text-sm md:text-md text-degrade tracking-widest">
+          <p className="text-xs md:text-md text-degrade tracking-widest">
             Copyright © 2023 - {currentYear}. All right reserved
           </p>
           <SocialComponent />
