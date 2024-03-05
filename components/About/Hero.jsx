8a7320ad5433e1c839c7ba74833f75d501491e98
jecode93 <jeanemmanuelcadet@gmail.com>
2024-03-04 20:46:26 -0500

    Create hero section for the about page.

---
+++ components/About/Hero.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import SectionsComponent from "../SectionsComponent";
+import SectionTitle from "../SectionTitle";
+import Image from "next/image";
+import me from "@/public//images/me.jpg";
+import { aboutMe } from "@/js/data/about/about";
+import SocialComponent from "../SocialComponent";
+
+const Hero = () => {
+  return (
+    <SectionsComponent id="about-me" otherClasses="pt-40">
+      <SectionTitle title="About Me" text="What to Know" />
+      <div className="flex flex-col-reverse items-center lg:flex-row justify-between gap-10">
+        <div className="flex justify-start lg:w-1/2">
+          {aboutMe.map((about, index) => (
+            <div key={index}>
+              <p className="description text-lg tracking-wide text-degrade text-justify">
+                {about.description}
+              </p>
+              <a
+                href="Jean_Emmanuel_Resume.pdf"
+                target="_blank"
+                aria-label="Download my resume"
+                className="px-3 py-2 my-5 border border-white bg-blue text-xs md:text-sm font-bold text-white uppercase hover:text-blue duration-300 btn41-43 btn-41"
+              >
+                Download Resume
+              </a>
+            </div>
+          ))}
+        </div>
+        <div className="lg:w-1/2 md:p-3">
+          <div className="flex flex-col items-center">
+            <Image
+              src={me}
+              className="w-full md:w-[500px] mb-4"
+              alt="Jean Emmanuel Cadet"
+              width={500}
+              height={500}
+            />
+            <div className="text-center">
+              <p className="text-xl md:text-2xl lg:text-3xl font-bold tracking-widest">
+                Jean Emmanuel CADET
+              </p>
+              <p className="text-lg md:text-xl text-degrade my-3">
+                Full-stack Software Engineer
+              </p>
+              <SocialComponent />
+            </div>
+          </div>
+        </div>
+      </div>
+    </SectionsComponent>
+  );
+};
+
+export default Hero;
---
+++ js/data/about/about.js
@@ -0,0 +1,14 @@
+export const aboutMe = [
+  {
+    title: "Jean Emmanuel CADET",
+    description: `I'm a passionate and experienced Full-stack Software Engineer with a proven track record of creating innovative software solutions for businesses of all sizes. 
+
+My expertise spans the entire development lifecycle, from crafting user-centric front-end experiences with technologies like React and Next.js to building robust, scalable back-ends using Ruby on Rails.
+
+Additionally, I'm proficient in managing databases like PostgreSQL, MySQL, and MongoDB, ensuring data integrity and accessibility throughout the development process.
+
+Beyond technical skills, I believe in delivering solutions that empower businesses to thrive. I partner with clients, whether startups or established enterprises, to understand their unique needs and develop software that streamlines processes, drives efficiency, and unlocks new growth opportunities with a focus on performance, reliability, and security. I ensure that every project exceeds expectations.
+
+I am not just building software - I am building solutions. Let's collaborate and shape the future of your business together.`,
+  },
+];
