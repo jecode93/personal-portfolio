318ace827bdf6cdd392fab1be5df10ea4d299e6e
jecode93 <jeanemmanuelcadet@gmail.com>
2024-03-04 20:45:03 -0500

    Create skills section for the about page.

---
+++ components/About/Skills.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import SectionsComponent from "../SectionsComponent";
+import SectionTitle from "../SectionTitle";
+import { skills } from "@/js/data/about/skills";
+import SkillsCard from "../SkillsCard";
+
+const Skills = () => {
+  return (
+    <SectionsComponent id="skills" otherClasses="">
+      <SectionTitle title="Technology" text="My Skills" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {
+          skills.map((skill) => (
+            <SkillsCard key={skill.id} content={skill} />
+          ))
+        }
+      </div>
+    </SectionsComponent>
+  );
+};
+
+export default Skills;
