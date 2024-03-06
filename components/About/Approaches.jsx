17e1f4658c5d66645adddafe5cacd99b26b6bdc0
jecode93 <jeanemmanuelcadet@gmail.com>
2024-03-06 12:14:13 -0500

    Display two approaches per column.

--- components/About/Approaches.jsx
+++ components/About/Approaches.jsx
@@ -8,7 +8,7 @@ const Approach = () => {
   return (
     <SectionsComponent id="approaches">
       <SectionTitle title="Approach" text="How I Work" />
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
         {approaches.map((approach) => (
           <ApproachCard key={approach.id} content={approach} />
         ))}
