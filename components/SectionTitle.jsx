d3f236f871db7bd47fc5669f83e712146a03b1bb
jecode93 <jeanemmanuelcadet@gmail.com>
2024-03-06 11:57:48 -0500

    Remove uppercase class and update font weight from black to extra bold.

--- components/SectionTitle.jsx
+++ components/SectionTitle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SectionTitle = ({ title, text }) => {
   return (
     <>
-      <h2 className="uppercase text-degrade text-[40px] md:-mb-4 md:-mt-16 font-black tracking-widest md:text-[85px] lg:text-[120px]">
+      <h2 className="text-degrade text-[50px] md:-mb-4 md:-mt-16 font-extrabold tracking-widest md:text-[85px] lg:text-[120px]">
         {title}
       </h2>
       <div className="mb-20 flex items-center">
