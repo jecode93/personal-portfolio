7e3cb0cfc9db8daf21489bb3152f5dc5487355e8
jecode93 <jeanemmanuelcadet@gmail.com>
2024-03-08 22:49:34 -0500

    Decrease font size.

--- components/SectionTitle.jsx
+++ components/SectionTitle.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const SectionTitle = ({ title, text }) => {
   return (
     <>
-      <h2 className="text-degrade text-[50px] md:-mb-4 md:-mt-16 font-extrabold tracking-widest md:text-[85px] lg:text-[120px]">
+      <h2 className="text-orange-500 text-[50px] md:-mb-2 md:-mt-16 font-extrabold tracking-widest md:text-[70px] lg:text-[100px]">
         {title}
       </h2>
       <div className="mb-20 flex items-center">
         <div className="w-12 mr-3 border border-orange-500" />
-        <h1 className="font-bold text-xl md:text-4xl tracking-wider">{text}</h1>
+        <h1 className="font-bold text-xl md:text-3xl tracking-wider">{text}</h1>
       </div>
     </>
   );
