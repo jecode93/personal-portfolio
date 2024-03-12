4778ec3b41c5be4a74ddc18b0b743da1a43910e6
jecode93 <jeanemmanuelcadet@gmail.com>
2024-03-12 16:52:47 -0400

    Remove unuse div.

--- components/ButtonComponent.jsx
+++ components/ButtonComponent.jsx
@@ -7,7 +7,6 @@ const ButtonComponent = ({ path, text }) => {
       href={path}
       className="w-fit text-xs md:text-sm mt-5 font-medium border border-white text-white hover:text-blue px-3 py-2 uppercase btn41-43 btn-41"
     >
-      <div className="absolute top-0 left-0 hidden hover:flex bg-orange-500 h-full w-full rounded"></div>
       {text}
     </Link>
   );
