50efc6fd0ed38f8a90e7ba66e251bde079a6f5f8
jecode93 <jeanemmanuelcadet@gmail.com>
2024-03-15 16:40:06 -0400

    Handle route to send email.

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import InfoCard from "./InfoCard";
 import { infos } from "@/js/data/contact/info";
-import { Resend } from "resend";
+import { POST } from "@/app/api/send/route";
 
 const ContactForm = () => {
   const {
@@ -15,27 +15,12 @@ const ContactForm = () => {
   const [successMessage, setSuccessMessage] = useState(null);
 
   const onSubmit = async (data) => {
-    try {
-      const resend = new Resend(process.env.NEXT_PUBLIC_API_KEY)
-      
+    try {      
       // Email content
-      const mailOptions = {
-        from: process.env.NEXT_PUBLIC_SMTP_EMAIL,
-        to: process.env.NEXT_PUBLIC_SMTP_RECIPIENT,
-        subject: "New Contact Form Submission | Jean Emmanuel Cadet",
-        html: `
-        <p>Name: ${data.name}</p>
-        <p>Company: ${data.company}</p>
-        <p>Email: ${data.email}</p>
-        <p>Phone: ${data.phone}</p>
-        <p>Message: ${data.message}</p>
-        <p>Service need: ${data.reason.join(", ")}</p>
-        <p>Budget: ${data.budget}</p>
-        `,
-      };
+      await POST(data)
 
-      // Send email
-      await resend.emails.send(mailOptions);
+      // // Send email
+      // await resend.emails.send(mailOptions);
 
       setSuccessMessage(
         "Thank you for reaching out to me, and I look forward to assisting you!"
