ed400bf66be1c69109a1b2158f8c016116fef22a
jecode93 <jeanemmanuelcadet@gmail.com>
2024-03-15 10:55:36 -0400

    Add contact form component.

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -7,25 +7,10 @@ import InfoCard from "../InfoCard";
 
 const ContactSection = () => {
   return (
-    <SectionsComponent id="contact" otherClasses="pt-32 md:pt-48">
+    <SectionsComponent id="contact" otherClasses="pt-32 md:pt-48 border-b border-degrade">
       <SectionTitle title="Contact Me" text="Shape your future with Me" />
       <div className="lg:flex justify-center gap-20">
-        <div className="lg:w-[40%] mb-10">
-          <h2 className="tracking-wide text-degrade text-justify mb-5">
-            <span className="font-bold">
-              Email, call, or complete the form{" "}
-            </span>
-            to learn how I can solve your problem and shape your future.
-          </h2>
-          <div>
-            {infos.map((info) => (
-              <InfoCard key={info.id} content={info} />
-            ))}
-          </div>
-        </div>
-        <div className="lg:w-[60%] mb-10">
-          <ContactForm />
-        </div>
+        <ContactForm />
       </div>
     </SectionsComponent>
   );
