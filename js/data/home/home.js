36f1365f0b70af542b523993084058dc9a311dc8
jecode93 <jeanemmanuelcadet@gmail.com>
2024-03-17 14:51:29 -0400

    Remove all size after business.

--- js/data/home/home.js
+++ js/data/home/home.js
@@ -3,7 +3,7 @@ export const hero = [
     subTitle: "Welcome to my digital space.",
     title: `I am a Full-stack
 Software Engineer`,
-    description: `I design, build, and deploy innovative software solutions for businesses of all sizes.`,
+    description: `I design, build, and deploy innovative software solutions for businesses.`,
   },
 ];
 
@@ -25,7 +25,7 @@ export const infosHero = [
 export const aboutme = [
   {
     title: "I'm Jean Emmanuel CADET",
-    description: `A Full-stack software engineer who specializes in building software solutions for businesses of all sizes using Front-end technologies like (JavaScript, TypeScript, React, Redux, and Next.js), Back-end technologies like (Ruby, Ruby on Rails, and NestJS), and Databases like (PostgreSQL, MySQL, and MongoDB).
+    description: `A Full-stack software engineer who specializes in building software solutions for businesses using Front-end technologies like (JavaScript, TypeScript, React, Redux, and Next.js), Back-end technologies like (Ruby, and Ruby on Rails), and Databases like (PostgreSQL, MySQL, and MongoDB).
       
 I bridge the gap between front-end and back-end development, bringing ideas to life with seamless user experiences and robust functionality.`,
   },
