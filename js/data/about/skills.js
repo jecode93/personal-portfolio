8c01b7e6887aac69aca504cb0598d63a9e6fbe24
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-05-04 02:04:39 -0400

    Change skills percentage.

--- js/data/about/skills.js
+++ js/data/about/skills.js
@@ -12,21 +12,21 @@ export const skills = [
   {
     id: 3,
     title: "React / Next.js",
-    percentage: "80",
+    percentage: "75",
   },
   {
     id: 4,
     title: "Ruby / Ruby on Rails",
-    percentage: "85",
+    percentage: "75",
   },
   {
     id: 5,
     title: "MySQL / PostgreSQL",
-    percentage: "80",
+    percentage: "75",
   },
   {
     id: 6,
     title: "RESTful API development",
-    percentage: "85",
+    percentage: "75",
   },
 ];
