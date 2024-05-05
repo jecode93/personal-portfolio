2c57cb889707d599a0b1852791ebff0bcf9056e0
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-05-05 17:29:37 -0400

    Center hero text on mobile version and adjust font size for mobile and desktop version.

--- components/Home/HeroSection.jsx
+++ components/Home/HeroSection.jsx
@@ -9,20 +9,20 @@ const Hero = () => {
       <div className="md:px-5 lg:px-32 2xl:px-80 h-screen flex flex-col justify-between">
         <div className="flex flex-col px-5 tracking-wide leading-7 justify-center h-full pt-16 z-0">
           {hero.map((text, index) => (
-            <div key={index}>
+            <div key={index} className="text-center md:text-left">
               <p className="md:text-lg text-orange-500">{text.greating}</p>
               <div className="font-bold my-5 description">
-                <h1 className="text-lightestSlate text-2xl md:text-6xl mb-3">
+                <h1 className="text-lightestSlate text-3xl md:text-6xl mb-3">
                   {text.title}
                 </h1>
-                <h2 className="text-slate text-2xl md:text-6xl">
+                <h2 className="text-slate text-2xl md:text-5xl">
                   {text.subTitle}
                 </h2>
               </div>
               <p className="mb-5 md:text-lg description md:w-[50%]">{text.description}</p>
+              <ButtonComponent path="/contact" text="Contact me" />
             </div>
           ))}
-          <ButtonComponent path="/contact" text="Contact me" />
         </div>
         {/* Social section */}
         <div className="socials-links px-5 z-0 mb-10">
