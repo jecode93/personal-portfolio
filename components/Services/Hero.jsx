19fa37c210850e82571db27c90711376d5ea62f7
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-05-05 15:14:47 -0400

    Update services hero text and add it dynamically.

--- components/Services/Hero.jsx
+++ components/Services/Hero.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import SectionsComponent from '../SectionsComponent';
-import SectionTitle from '../SectionTitle';
+import React from "react";
+import SectionsComponent from "../SectionsComponent";
+import SectionTitle from "../SectionTitle";
+import { hero } from "@/js/data/services/hero";
 
 const Hero = () => {
   return (
     <SectionsComponent id="services" otherClasses="pt-32 md:pt-48 -mb-20">
       <SectionTitle title="Services" text="What I Build" />
       <div className="-mt-7">
-        <p className="tracking-wide text-degrade text-justify">
-          I offer a comprehensive range of services designed to bring your
-          digital ideas to life and enhance your online presence.
-          <br />
-          <br />
-          With a strong background in both front-end and back-end development, I
-          am dedicated to delivering robust, scalable, and user-centric
-          solutions tailored to meet your unique business needs.
-        </p>
+          {hero.map((data) => (
+            <div key={data}>
+              <p className="tracking-wide text-slate text-justify description">
+                {data.services}
+              </p>
+            </div>
+          ))}
       </div>
     </SectionsComponent>
   );
-}
+};
 
 export default Hero;
