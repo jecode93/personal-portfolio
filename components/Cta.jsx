8df2496a54b6c84f0ce849fdf42e52b7fbda0d8e
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-05-05 16:16:25 -0400

    Adjust padding y to 1 instead of 2.

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -24,7 +24,7 @@ const Cta = () => {
       </p>
       <Link
         href="contact"
-        className="px-3 py-2 bg-blue text-xs md:text-sm font-medium text-lightestSlate uppercase hover:text-blue duration-300 btn41-43 btn-41"
+        className="px-3 py-1 bg-blue text-xs md:text-sm font-medium text-lightestSlate uppercase hover:text-blue duration-300 btn41-43 btn-41"
       >
         Start today
       </Link>
