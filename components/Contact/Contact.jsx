a3e454a567ccee9930e4def9fa26a51c71c52a7e
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-05-10 03:35:51 -0400

    Change contact page display, and add new text.

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import SectionsComponent from "../utils/SectionsComponent";
 import SectionTitle from "../utils/SectionTitle";
 import ContactForm from "./ContactForm";
+import InfoCard from "../utils/InfoCard";
+import { infos } from "@/js/data/contact/info";
 
 const ContactSection = () => {
   return (
@@ -10,8 +12,29 @@ const ContactSection = () => {
       otherClasses="pt-32 md:pt-48 border-b border-degrade"
     >
       <SectionTitle title="Contact Me" text="Shape your future with Me" />
-      <div className="lg:flex justify-center gap-20">
-        <ContactForm />
+      <div className="flex flex-col justify-center items-center">
+        <div className="lg:flex justify-between gap-5">
+          <div className="mb-10 lg:w-1/2">
+            <h2 className="text-2xl text-lightestSlate uppercase font-bold mb-5">
+              Let&apos;s Talk For your{" "}
+              <span className="text-orange-500">Next Project(s)</span>
+            </h2>
+            <h2 className="tracking-wide text-justify mb-5">
+              <span className="font-bold">
+                Email, call, or complete the form{" "}
+              </span>
+              to learn how I can solve your problem and shape your future.
+            </h2>
+            <div>
+              {infos.map((info) => (
+                <InfoCard key={info.id} content={info} />
+              ))}
+            </div>
+          </div>
+          <div className="lg:w-1/2">
+            <ContactForm />
+          </div>
+        </div>
       </div>
     </SectionsComponent>
   );
