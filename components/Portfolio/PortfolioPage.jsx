6b54d077a0389d7d484e86ffacec3af0f09c3791
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-06-14 00:10:35 -0400

    Add content in portfolio page.

---
+++ components/Portfolio/PortfolioPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { portfolios } from "@/js/data/home/portfolio";
+import PortfolioCard from "../utils/PortfolioCard";
+import SectionsComponent from "../utils/SectionsComponent";
+import SectionTitle from "../utils/SectionTitle";
+
+const PortfolioPage = () => {
+  return (
+    <SectionsComponent id="Portfolio" otherClasses="pt-32 md:pt-48">
+      <SectionTitle title="My portfolio" text="Explore My Popular Projects" />
+      {portfolios.map((portfolio, index) => (
+        <div key={portfolio.id} className="duration-500">
+          <PortfolioCard content={portfolio} index={index} />
+        </div>
+      ))}
+    </SectionsComponent>
+  );
+};
+
+export default PortfolioPage;
