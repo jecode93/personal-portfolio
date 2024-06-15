291ae0cb2e01915df04ff1f7394f9880f85a323a
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-06-14 23:42:47 -0400

    Add chevron icon to buttons.

--- components/utils/ButtonComponent.jsx
+++ components/utils/ButtonComponent.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import { FaChevronRight } from "react-icons/fa";
 
 const ButtonComponent = ({ path, text }) => {
   return (
@@ -7,7 +8,10 @@ const ButtonComponent = ({ path, text }) => {
       href={path}
       className="w-fit text-xs md:text-sm mt-5 font-medium border border-lightestSlate text-lightestSlate hover:text-blue px-5 py-2 uppercase btn41-43 btn-41"
     >
-      {text}
+      <div className="flex gap-3 items-center">
+        {text}
+        <FaChevronRight />
+      </div>
     </Link>
   );
 };
