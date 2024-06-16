36696777da4a0229759891bb537c1d1ff7513a88
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-06-16 15:38:31 -0400

    Remove line break in portfolio description.

--- js/data/home/portfolio.js
+++ js/data/home/portfolio.js
@@ -7,23 +7,16 @@ export const portfolios = [
     description: `Car Book revolutionizes car rentals, bridging the gap between renters and owners with a user-centric platform.
 
 Streamlined Rentals for Renters:
-
 • Effortless Browsing: Explore a diverse selection of cars based on your specific needs, filtering by type, size, and features.
-
 • Informed Decisions: Make confident choices with detailed car information, high-quality images, and transparent pricing.
-
 • Seamless Booking: Secure your ideal car with a user-friendly reservation system, ensuring a smooth rental experience.
 
 Empowering Car Owners:
-
 • Centralized Management: Manage listings, set availability, and track reservations – all from a convenient dashboard.
-
 • Effortless Listing: Add your car with ease, providing detailed descriptions and captivating photos.
-
 • Building Trust: Foster transparency with features like car addition/removal and user reviews.
 
 Beyond Convenience:
-
 Car Book transcends a simple booking platform, fostering a community built on trust and transparency. It empowers both renters and owners, saving time, simplifying the process, and offering a seamless experience for all.`,
     resume: "Streamlined car rentals for renters.",
     stack: [
@@ -44,7 +37,6 @@ Car Book transcends a simple booking platform, fostering a community built on tr
     description: `Space Travelers' Hub (STH) is a dynamic single-page application (SPA) that ignites your dreams of space exploration. Powered by real-time data from the SpaceX API, STH empowers you to:
 
 • Explore and book rockets: Browse through available SpaceX rockets, delve into their specifications, and seamlessly secure your reservation for an unforgettable celestial journey.
-
 • Join captivating missions: Discover a curated selection of thrilling space missions and become an active participant in pushing the boundaries of human exploration.
 
 STH transcends a simple booking platform, fostering a vibrant community of space enthusiasts united by a shared passion for discovery.`,
@@ -59,13 +51,10 @@ STH transcends a simple booking platform, fostering a vibrant community of space
     description: `This captivating single-page application (SPA) caters to every weather enthusiast, providing a comprehensive and convenient platform to explore a world of meteorological data.
 
 Key Features:
-
 • Global Reach: Delve into weather conditions across the globe, choosing any country to unlock a treasure trove of insights.
-
 • Granular Details: Dive deeper into each chosen country, accessing real-time weather metrics for its capital city, including temperature, wind speed, humidity, and more.
 
 Beyond Information:
-
 This user-friendly app empowers you to stay informed and make informed decisions, whether planning a trip, preparing for outdoor activities, or simply staying curious about the ever-changing world around you.`,
     resume: "Explore global weather & real-time details.",
     stack: ["JavaScript", "React", "Redux", "Tailwind CSS"],
@@ -87,15 +76,11 @@ This user-friendly app empowers you to stay informed and make informed decisions
     description: `This compelling JavaScript project, built with cutting-edge Webpack and ES6 features, unveils a dynamic leaderboard experience. It seamlessly retrieves data from the Leaderboard API using the power of asynchronous operations (async/await), ensuring a smooth and efficient user experience.
 
 Key Features:
-
 • Live Score Display: Witness your scores prominently displayed, keeping you motivated and informed about your progress.
-
 • Modern Tech Stack: Experience the benefits of Webpack and ES6, ensuring a robust and future-proof development foundation.
-
 • Asynchronous Data Fetching: Enjoy seamless data retrieval from the Leaderboard API, powered by efficient async/await functionality.
 
 Beyond Scores:
-
 This project transcends a simple score display, fostering a sense of competition and community among users striving to reach the top.`,
     resume: "Add scores, and data fetching.",
     stack: ["HTML", "CSS", "JavaScript", "Webpack", "SCSS"],
@@ -108,7 +93,6 @@ This project transcends a simple score display, fostering a sense of competition
     description: `Math Magicians is a captivating single-page application (SPA) designed to ignite your passion for mathematics. This interactive platform caters to learners of all levels, offering two key features:
 
 • Effortless Calculations: Perform basic mathematical operations with ease, making it a handy tool for quick calculations or practicing your numerical skills.
-
 • Daily Dose of Inspiration: Discover a new thought-provoking math quote each day, sparking your curiosity and igniting your fascination with the beauty and power of numbers.
 
 Math Magicians goes beyond a simple calculator, fostering a love for mathematics and encouraging exploration in a fun and engaging way.`,
@@ -123,13 +107,10 @@ Math Magicians goes beyond a simple calculator, fostering a love for mathematics
     description: `This website serves as a captivating platform, meticulously crafted to showcase your client's exceptional work and seamlessly facilitate product sales. It caters to both potential clients and loyal customers, offering a comprehensive and engaging experience.
 
 Key Features:
-
 • Dazzling Portfolio: Immerse visitors in a stunning display of your client's work, highlighting their expertise and creative vision.
-
 • Seamless Shopping Experience: Navigate a user-friendly interface designed to streamline the product purchasing process, ensuring customer satisfaction.
 
 Beyond Functionality:
-
 This website transcends a simple online store, fostering a lasting connection between your client and their audience. It serves as a powerful tool to build brand recognition, cultivate customer loyalty, and drive successful sales.`,
     resume: "Client's portfolio & seamless product sales.",
     stack: ["WordPress", "Elementor Page Builder"],
@@ -142,13 +123,10 @@ This website transcends a simple online store, fostering a lasting connection be
     description: `Three years ago, I created a dynamic platform to empower informed citizens, and this client's news blog stands as a testament to that vision. This comprehensive platform transcends a simple news feed, offering a robust suite of features:
 
 • National and International News: Stay abreast of current events, both locally and globally, with access to a diverse range of news articles.
-
 • Content Management Powerhouse: Enjoy the flexibility to create, edit, and delete posts, ensuring the blog remains current and relevant.
-
 • Collaborative Spirit: Foster a collaborative environment by adding other users to the management team, allowing for diverse perspectives and efficient content creation.
 
 Beyond the Headlines:
-
 This blog serves not only as a source of information but also as a platform for open dialogue and critical thinking. It empowers readers to engage with the news, form informed opinions, and participate in meaningful discussions.`,
     resume: "News blog with national & international coverage.",
     stack: ["WordPress", "Elementor Page Builder"],
