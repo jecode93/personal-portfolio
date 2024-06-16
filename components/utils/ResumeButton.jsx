8c7f6e7de8a8aff24981e7108e89b67dba841fd2
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-06-16 15:36:38 -0400

    Fix resume path error.

--- components/utils/ResumeButton.jsx
+++ components/utils/ResumeButton.jsx
@@ -4,7 +4,7 @@ const ResumeButton = () => {
   return (
     <>
       <a
-        href="resume.pdf"
+        href="/resume.pdf"
         target="_blank"
         aria-label="Download my resume"
         className="px-5 py-2 border border-lightestSlate bg-blue text-xs md:text-sm font-medium text-lightestSlate uppercase hover:text-blue transition duration-500 btn41-43 btn-41"
