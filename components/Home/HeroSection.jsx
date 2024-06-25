7aeb06307e0ee2dfdc573cc680cebd6f37c5f2d0
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-06-25 13:50:20 -0400

    Add simple animation in hero section of homepage.

--- components/Home/HeroSection.jsx
+++ components/Home/HeroSection.jsx
@@ -1,42 +1,76 @@
+"use client";
 import React from "react";
 import { hero } from "@/js/data/home/home";
 import ButtonComponent from "../utils/ButtonComponent";
 import SocialComponent from "../utils/SocialComponent";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
+const variants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.25,
+    },
+  },
+};
+
+const unitVariants = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
 
 const Hero = () => {
   return (
-    <section id="home" className="w-[100%] h-[100%] text-lightestSlate">
-      <div className="md:px-5 lg:px-32 2xl:px-80 h-screen flex flex-col justify-between">
-        <div className="flex flex-col px-5 tracking-wide leading-7 justify-center h-full pt-16 z-0">
+    <motion.section
+      variants={variants}
+      initial="hidden"
+      animate="show"
+      id="home"
+      className="w-[100%] h-[100%] text-lightestSlate"
+    >
+      <motion.div className="md:px-5 lg:px-32 2xl:px-80 h-screen flex flex-col justify-between">
+        <motion.div className="flex flex-col px-5 tracking-wide leading-7 justify-center h-full pt-16 z-0">
           {hero.map((content, index) => (
-            <div
+            <motion.div
               key={index}
               className="flex items-center justify-between gap-16"
             >
-              <div className="flex flex-col items-start">
-                <div className="text-center lg:text-left mb-10">
-                  <p className="md:text-lg">{content.greating}</p>
-                  <div className="font-extrabold my-5 description">
-                    <h1 className="text-orange-500 text-[1.4rem] mb-3 md:text-3xl lg:text-[2.5rem] lg:mb-5 uppercase">
+              <motion.div className="flex flex-col items-start">
+                <motion.div className="text-center lg:text-left mb-10">
+                  <motion.p variants={unitVariants} className="md:text-lg">
+                    {content.greating}
+                  </motion.p>
+                  <motion.div className="font-extrabold my-5 description">
+                    <motion.h1
+                      variants={unitVariants}
+                      className="text-orange-500 text-[1.4rem] mb-3 md:text-3xl lg:text-[2.5rem] lg:mb-5 uppercase"
+                    >
                       {content.title}{" "}
                       <span className="text-lightestSlate">CADET</span>
-                    </h1>
-                    <h2 className="text-[1.4rem] font-semibold md:text-3xl lg:text-[2.1rem]">
+                    </motion.h1>
+                    <motion.h2
+                      variants={unitVariants}
+                      className="text-[1.4rem] font-semibold md:text-3xl lg:text-[2.1rem]"
+                    >
                       {content.subTitle}
-                    </h2>
-                  </div>
-                  <p className="mb-5 md:text-lg description">
+                    </motion.h2>
+                  </motion.div>
+                  <motion.p
+                    variants={unitVariants}
+                    className="mb-5 md:text-lg description"
+                  >
                     {content.description}
-                  </p>
+                  </motion.p>
                   <ButtonComponent path="/contact" text="Contact me" />
-                </div>
+                </motion.div>
 
-                <div className="hidden lg:flex">
+                <motion.div variants={unitVariants} className="hidden lg:flex">
                   <SocialComponent className="lg:text-3xl mr-3" />
-                </div>
-              </div>
-              <div className="hidden lg:flex">
+                </motion.div>
+              </motion.div>
+              <motion.div variants={unitVariants} className="hidden lg:flex">
                 <Image
                   src="/developer.svg"
                   width={70}
@@ -44,17 +78,20 @@ const Hero = () => {
                   alt={content.title}
                   className="mb-7 w-[50rem]"
                 />
-              </div>
-            </div>
+              </motion.div>
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
         {/* Social section */}
-        <div className="socials-links lg:hidden text-left px-5 z-0 mb-10">
+        <motion.div
+          variants={unitVariants}
+          className="socials-links lg:hidden text-left px-5 z-0 mb-10"
+        >
           <SocialComponent />
-        </div>
+        </motion.div>
         {/* End Social section */}
-      </div>
-    </section>
+      </motion.div>
+    </motion.section>
   );
 };
 
