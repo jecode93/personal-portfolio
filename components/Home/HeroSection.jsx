604f5d63269efbd64bfe31cd8e94e2e583d0e3a7
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-07-08 11:08:37 -0400

    Remove animations in the website.

--- components/Home/HeroSection.jsx
+++ components/Home/HeroSection.jsx
@@ -4,73 +4,40 @@ import { hero } from "@/js/data/home/home";
 import ButtonComponent from "../utils/ButtonComponent";
 import SocialComponent from "../utils/SocialComponent";
 import Image from "next/image";
-import { motion } from "framer-motion";
-
-const variants = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.25,
-    },
-  },
-};
-
-const unitVariants = {
-  hidden: { opacity: 0 },
-  show: { opacity: 1 },
-};
 
 const Hero = () => {
   return (
-    <motion.section
-      variants={variants}
-      initial="hidden"
-      animate="show"
-      id="home"
-      className="w-[100%] h-[100%] text-lightestSlate"
-    >
-      <motion.div className="md:px-5 lg:px-32 2xl:px-80 h-screen flex flex-col justify-between">
-        <motion.div className="flex flex-col px-5 tracking-wide leading-7 justify-center h-full pt-16 z-0">
+    <section id="home" className="w-[100%] h-[100%] text-lightestSlate">
+      <div className="md:px-5 lg:px-32 2xl:px-80 h-screen flex flex-col justify-between">
+        <div className="flex flex-col px-5 tracking-wide leading-7 justify-center h-full pt-16 z-0">
           {hero.map((content, index) => (
-            <motion.div
+            <div
               key={index}
               className="flex items-center justify-between gap-16"
             >
-              <motion.div className="flex flex-col items-start">
-                <motion.div className="text-center lg:text-left mb-10">
-                  <motion.p variants={unitVariants} className="md:text-lg">
-                    {content.greating}
-                  </motion.p>
-                  <motion.div className="font-extrabold my-5 description">
-                    <motion.h1
-                      variants={unitVariants}
-                      className="text-orange-500 text-[1.4rem] mb-3 md:text-3xl lg:text-[2.5rem] lg:mb-5 uppercase"
-                    >
+              <div className="flex flex-col items-start">
+                <div className="text-center lg:text-left mb-10">
+                  <p className="md:text-lg">{content.greating}</p>
+                  <div className="font-extrabold my-5 description">
+                    <h1 className="text-orange-500 text-[1.4rem] mb-3 md:text-3xl lg:text-[2.5rem] lg:mb-5 uppercase">
                       {content.title}{" "}
                       <span className="text-lightestSlate">CADET</span>
-                    </motion.h1>
-                    <motion.h2
-                      variants={unitVariants}
-                      className="text-[1.4rem] font-semibold md:text-3xl lg:text-[2.1rem]"
-                    >
+                    </h1>
+                    <h2 className="text-[1.4rem] font-semibold md:text-3xl lg:text-[2.1rem]">
                       {content.subTitle}
-                    </motion.h2>
-                  </motion.div>
-                  <motion.p
-                    variants={unitVariants}
-                    className="mb-5 md:text-lg description"
-                  >
+                    </h2>
+                  </div>
+                  <p className="mb-5 md:text-lg description">
                     {content.description}
-                  </motion.p>
+                  </p>
                   <ButtonComponent path="/contact" text="Contact me" />
-                </motion.div>
+                </div>
 
-                <motion.div variants={unitVariants} className="hidden lg:flex">
+                <div className="hidden lg:flex">
                   <SocialComponent className="lg:text-3xl mr-3" />
-                </motion.div>
-              </motion.div>
-              <motion.div variants={unitVariants} className="hidden lg:flex">
+                </div>
+              </div>
+              <div className="hidden lg:flex">
                 <Image
                   src="/developer.svg"
                   width={70}
@@ -78,20 +45,17 @@ const Hero = () => {
                   alt={content.title}
                   className="mb-7 w-[50rem]"
                 />
-              </motion.div>
-            </motion.div>
+              </div>
+            </div>
           ))}
-        </motion.div>
+        </div>
         {/* Social section */}
-        <motion.div
-          variants={unitVariants}
-          className="socials-links lg:hidden text-left px-5 z-0 mb-10"
-        >
+        <div className="socials-links lg:hidden text-left px-5 z-0 mb-10">
           <SocialComponent />
-        </motion.div>
+        </div>
         {/* End Social section */}
-      </motion.div>
-    </motion.section>
+      </div>
+    </section>
   );
 };
 
--- components/Services/Hero.jsx
+++ components/Services/Hero.jsx
@@ -1,39 +1,21 @@
-'use client'
 import React from "react";
 import SectionsComponent from "../utils/SectionsComponent";
 import SectionTitle from "../utils/SectionTitle";
 import { hero } from "@/js/data/services/hero";
-import { motion } from "framer-motion";
-
-
-const variants = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.25,
-    },
-  },
-};
-
-const unitVariants = {
-  hidden: { opacity: 0 },
-  show: { opacity: 1 },
-};
 
 const Hero = () => {
   return (
     <SectionsComponent id="services" otherClasses="pt-32 md:pt-48 -mb-20">
       <SectionTitle title="Services" text="What I Build" />
-      <motion.div variants={variants} initial="hidden" animate="show" className="-mt-7">
+      <div className="-mt-7">
         {hero.map((data) => (
-          <motion.div key={data}>
-            <motion.p variants={unitVariants} className="tracking-wide text-slate text-justify description">
+          <div key={data}>
+            <p className="tracking-wide text-slate text-justify description">
               {data.services}
-            </motion.p>
-          </motion.div>
+            </p>
+          </div>
         ))}
-      </motion.div>
+      </div>
     </SectionsComponent>
   );
 };
