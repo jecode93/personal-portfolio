da4795159e68bcdd2bdbe3ca99c75f7366c907a8
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-07-12 15:56:53 -0700

    Add icon, remove font-bold and reduce gap between text and icon.

--- components/utils/BookingCalendly.jsx
+++ components/utils/BookingCalendly.jsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import React from "react";
+import { FaCalendarAlt } from "react-icons/fa";
 
 const BookingCalendly = ({ path, text }) => {
   return (
     <div>
       <Link
         href={path}
-        className="w-fit text-xs md:text-sm mt-5 font-bold bg-orange-500 px-5 py-2 uppercase btn43-43 btn-43"
+        className="w-fit text-xs md:text-sm mt-5 bg-orange-500 px-5 py-2 uppercase btn43-43 btn-43"
       >
-        <div className="flex gap-3 items-center">
+        <div className="flex gap-2 items-center">
           {text}
+          <FaCalendarAlt />
         </div>
       </Link>
     </div>
