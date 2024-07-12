15ec2852b9e521c9c3ae7d75f5d881b5577da430
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-07-12 16:04:07 -0700

    Remove font-medium and reduce gap.

--- components/utils/PreviewButton.jsx
+++ components/utils/PreviewButton.jsx
@@ -7,7 +7,7 @@ const PreviewButton = ({link, title, text, icon}) => {
         href={link}
         target="_blank"
         aria-label={title}
-        className="px-5 py-2 border border-lightestSlate bg-blue text-xs md:text-sm font-medium text-lightestSlate hover:text-blue transition duration-500 btn41-43 btn-41"
+        className="px-5 py-2 border border-lightestSlate bg-blue text-xs md:text-sm text-lightestSlate hover:text-blue transition duration-500 btn41-43 btn-41"
       >
         <div className="flex items-center">
           <p className="mr-3 tracking-wide">{text}</p>
--- components/utils/ResumeButton.jsx
+++ components/utils/ResumeButton.jsx
@@ -7,7 +7,7 @@ const ResumeButton = () => {
         href="/resume.pdf"
         target="_blank"
         aria-label="Download my resume"
-        className="px-5 py-2 border border-lightestSlate bg-blue text-xs md:text-sm font-medium text-lightestSlate uppercase hover:text-blue transition duration-500 btn41-43 btn-41"
+        className="px-5 py-2 border border-lightestSlate bg-blue text-xs md:text-sm text-lightestSlate uppercase hover:text-blue transition duration-500 btn41-43 btn-41"
       >
         Resume
       </a>
